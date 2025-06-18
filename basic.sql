623ac59a145a1319b57b6f9065fd9bdd961a8f48
k-hyeongyu <kampher@naver.com>
2025-06-18 16:52:07 +0900

    내용 추가 및 생성, 선생님 코드 복붙

--- basic.sql
+++ basic.sql
@@ -1,25 +1,25 @@
 
 --SQL Query 주석
 --한줄주석
+/* 여러줄 주석
+여러줄 주석*/
 
-/*여러줄
-주석*/
+DB (DataBase) 데이터가 저장 공간 
+DBMS (DataBase Management System) 데이터베이스를 관리하는 시스템
 
-DB (Database) 데이터가 저장공간
-DBMS (Database Management System) 데이터베이스 관리 시스템
-
-Windows os -> Oracle DBMS <--- sql developer / sqlPlus 등의 툴을 사용하여 사용자가 접속 후 사용
+Windows OS -> Oracle DBMS <-- sqldeveloper / sqlplus 등등 툴을 활용해서 사용자가 접속 후 사용
                             (SQL 쿼리로 DB 데이터를 사용(조작))
-                            
-*접속계정
+
+*접속 계정
 id : scott
 pw : tiger
 
 SQL (쿼리문)
 Structured Query Language
 
-DB사용(조작) -> 데이터 조작 쿼리문 작성
-Create 생서
-Read 읽기 조회
+DB 사용(조작) -> 데이터 조작 (쿼리문을 작성해서)
+CRUD
+Create 생성
+Read   조회(읽기)
 Update 수정
 Delete 삭제
