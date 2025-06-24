7e9faa42c0f9554165b6f56727fd700a7821a965
k-hyeongyu <kampher@naver.com>
2025-06-24 09:33:42 +0900

    연습22 쿼리

---
+++ practice/practice22.sql
@@ -0,0 +1,36 @@
+--1)'AL Pacino'와 "같은 지역"에서
+--근무하는 직원들의 평균 연봉보다
+--많이 받는 직원들의 사번, 이름, 부서번호, 부서이름, 근무지역, 급여 를 출력하세요.
+
+select * from emp2;
+select * from dept2;
+
+select  empno, name, deptno, dname, area, pay
+from emp2 e, dept2 d
+where 
+    e.deptno = d.dcode
+and e.pay > (select avg(pay)
+            from emp2
+            where deptno in ( select dcode
+                                from dept2
+                                where area = (  select area
+                                                from dept2
+                                                where dcode = (
+                                                                select deptno
+                                                                from emp2
+                                                                where name = 'AL Pacino'))));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
